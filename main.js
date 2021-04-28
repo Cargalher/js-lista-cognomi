d235b548827f0c1d0cddba2a33ef1a4c0908f6f8
Cargalher <cargalher@gmail.com>
2021-04-28 15:48:34 +0200

    checking console

--- main.js
+++ main.js
@@ -7,6 +7,9 @@
 
 // 1. chiedi all’utente il cognome_ taking the input from the user and saving it as a variable.
 var Usersurname = prompt("What's your Surname?");
-
+console.log("Usersurname");
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’_ declaring an array of surnames
 var surnames = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+console.log("surnames");
+console.log(surnames);
+// surnames.push(Usersurname);  //with this I add the Usersurname, I asked for, to the Array surnames.

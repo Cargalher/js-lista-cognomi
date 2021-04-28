da41123b58a0b47a4bd526a645bed5ddfcf9c6e6
Cargalher <cargalher@gmail.com>
2021-04-28 15:37:40 +0200

    declaring an array of surnames

--- main.js
+++ main.js
@@ -5,5 +5,8 @@
 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 */
 
-// 1. chiedi all’utente il cognome
+// 1. chiedi all’utente il cognome_ taking the input from the user and saving it as a variable.
 var Usersurname = prompt("What's your Surname?");
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’_ declaring an array of surnames
+var surnames = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
